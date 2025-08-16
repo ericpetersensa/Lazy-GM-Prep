c4a3c0b3fac77bfb8943102fe2b6c219c965c4c0
ericpetersensa <ericpetersensa@gmail.com>
2025-08-15 20:28:49 -0700

    Create lazy-gm-prep.js

---
+++ scripts/lazy-gm-prep.js
@@ -0,0 +1,19 @@
+// lazy-gm-prep.js
+
+import { registerSettings } from "./scripts/settings.js";
+import { createLazyGMJournal } from "./scripts/steps.js";
+import { LazyGMPrepApp } from "./scripts/app.js"; // Assuming you have this already
+
+Hooks.once("init", () => {
+  console.log("Lazy GM Prep | Initializing module");
+  registerSettings();
+});
+
+Hooks.once("ready", () => {
+  if (game.user.isGM && game.settings.get("lazy-gm-prep", "autoCreateJournal")) {
+    createLazyGMJournal();
+  }
+
+  // Optional: render the app on startup
+  // new LazyGMPrepApp().render(true);
+});
