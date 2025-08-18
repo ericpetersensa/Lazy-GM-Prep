9990a58fb8ded1563431a4118218e8a10977cc3e
Eric Petersen <ericpetersensa@gmail.com>
2025-08-18 11:11:38 -0700

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -4,7 +4,22 @@ import { LazyGMPrepApp } from "./app.js";
 
 export const MODULE_ID = "lazy-gm-prep";
 
+/**
+ * Registers the settings and menu for Lazy GM Prep.
+ * Ensures visibility in Game Settings > Configure Settings.
+ */
 export function registerSettings() {
+  // Dummy setting to anchor the settings menu in the UI
+  game.settings.register(MODULE_ID, "dummy", {
+    name: "Lazy GM Prep",
+    hint: "Launch the Lazy GM Prep interface.",
+    scope: "world",
+    config: true,
+    type: Boolean,
+    default: false
+  });
+
+  // Menu registration for launching the Lazy GM Prep app
   game.settings.registerMenu(MODULE_ID, "lazy-gm-prep-menu", {
     name: "Lazy GM Prep",
     label: "Lazy GM Prep",
